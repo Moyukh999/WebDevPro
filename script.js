cd2342cdd0f6cdc6fb35d7083211b1e9de789257
Moyukh999 <142529951+Moyukh999@users.noreply.github.com>
2024-10-28 00:09:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function makeProject() {
+    document.getElementById('projectMessage').textContent = "Great! Start your first project by applying what you’ve learned.";
+}
+
+function navigateTo(page) {
+    window.location.href = page;
+}
